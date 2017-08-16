9417ea4bb98a6856510173ae6507cc86dc47aecd
FDeityLink <brianchristian16@gmail.com>
2017-08-16 15:48:06 -0400

    Turned NullArgumentException.requireNonNull's three overloads into top-level functions

--- src/main/kotlin/io/fdeitylink/util/NullArgumentException.kt
+++ src/main/kotlin/io/fdeitylink/util/NullArgumentException.kt
@@ -1,19 +1,16 @@
 package io.fdeitylink.util
 
-class NullArgumentException(funcName: String, argName: String):
+class NullArgumentException(funcName: String, argName: String) :
         NullPointerException("Null value passed for argument $argName to function " +
                              "${funcName + if ("" == funcName) "" else "()"} when a null value is not permitted") {
 
-    constructor(): this("", "")
-    constructor(funcName: String): this(funcName, "")
-
-    companion object {
-        fun <T> requireNonNull(obj: T?) = requireNonNull(obj, "", "")
-
-        fun <T> requireNonNull(obj: T, funcName: String) = requireNonNull(obj, funcName, "")
-
-        fun <T> requireNonNull(obj: T, funcName: String, argName: String): T {
-            return obj ?: throw NullArgumentException(funcName, argName)
-        }
-    }
+    constructor() : this("", "")
+    constructor(funcName: String) : this(funcName, "")
 }
+
+fun <T> requireNonNull(obj: T?) = requireNonNull(obj, "", "")
+
+fun <T> requireNonNull(obj: T, funcName: String) = requireNonNull(obj, funcName, "")
+
+fun <T> requireNonNull(obj: T, funcName: String, argName: String) =
+        obj ?: throw NullArgumentException(funcName, argName)
