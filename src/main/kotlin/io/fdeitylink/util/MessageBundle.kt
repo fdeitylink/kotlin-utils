da4b0d5393828c474549f9eb3aa0ce9f55ff350d
FDeityLink <brianchristian16@gmail.com>
2017-08-17 23:39:02 -0400

    Improved exception suppression for MessageBundle: Handles nonexistent ResourceBundles and keys that don't map to Strings MessageBundle.Companion.bundle is now Map with private HashMap backing property Improved MessageBundle documentation

--- src/main/kotlin/io/fdeitylink/util/MessageBundle.kt
+++ src/main/kotlin/io/fdeitylink/util/MessageBundle.kt
@@ -17,31 +17,67 @@
 package io.fdeitylink.util
 
 import java.util.ResourceBundle
+import java.util.MissingResourceException
 
-class MessageBundle(val bundleName: String) {
-    private val resourceBundle = ResourceBundle.getBundle(bundleName)
+/**
+ * Wraps a [ResourceBundle] and provides a convenient [get] operator method.
+ *
+ * The class also suppresses [ClassCastExceptions][ClassCastException] and
+ * [MissingResourceExceptions][MissingResourceException] should they occur when attempting to retrieve a bundle
+ * or one of its values.
+ *
+ * @constructor
+ * Constructs a new [MessageBundle] that wraps a [ResourceBundle] with the given [bundleName]
+ *
+ * If no bundle exists for the given [bundleName], then the get [instance method][get] and
+ * [companion object method][MessageBundle.Companion.get] will always return the key they are given
+ */
+class MessageBundle(val bundleName: String)  {
+    @Suppress("JoinDeclarationAndAssignment")
+    private val resourceBundle: ResourceBundle?
 
     init {
-        allBundles[bundleName] = this
+        resourceBundle = try {
+            ResourceBundle.getBundle(bundleName)
+        }
+        catch (except: MissingResourceException) {
+            null
+        }
+
+        _bundles[bundleName] = this
     }
 
     /**
-     * Returns a [String] that corresponds to [key] in the resource bundle specified
-     * by [bundleName], or [key] if there is no corresponding [String]
+     * Returns a [String] that corresponds to [key] in the resource bundle specified by [bundleName],
+     * or [key] if no [ResourceBundle] with the name [bundleName] exists or there is no corresponding
+     * [String] in the [ResourceBundle] that does exist
      */
-    operator fun get(key: String): String = if (resourceBundle.containsKey(key)) resourceBundle.getString(key) else key
+    operator fun get(key: String): String {
+        return try {
+            if (true == resourceBundle?.containsKey(key)) resourceBundle.getString(key) else key
+        }
+        catch (except: ClassCastException) {
+            key
+        }
+    }
 
     companion object {
+        @JvmStatic
+        private val _bundles = hashMapOf<String, MessageBundle>()
+
         /**
-         * A [HashMap] mapping each instantiated [MessageBundle's][MessageBundle] [bundleName] to itself
+         * A [Map] mapping each instantiated [MessageBundle's][MessageBundle] [bundleName] to itself
          */
-        val allBundles = hashMapOf<String, MessageBundle>()
+        @JvmStatic
+        val bundles: Map<String, MessageBundle>
+            get() = _bundles
 
         /**
          * Returns a [String] that corresponds to [key] in the resource bundle specified by [bundleName],
-         * or [key] if no [MessageBundle] exists with the given [bundleName] or if no [String] corresponds
-         * to [key] in an existing [MessageBundle]
+         * or [key] if no [ResourceBundle] with the name [bundleName] exists or there is no corresponding
+         * [String] in the [ResourceBundle] that does exist
          */
-        operator fun get(bundleName: String, key: String) = allBundles[bundleName]?.get(key) ?: key
+        @JvmStatic
+        operator fun get(bundleName: String, key: String) = bundles[bundleName]?.get(key) ?: key
     }
 }
