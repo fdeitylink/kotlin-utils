2e4dfccce17566a00f67a236477d6fa4622f28ba
fdeitylink <brianchristian16@gmail.com>
2017-08-19 16:31:21 -0400

    Removed removeJavadocJar Kobalt task (it's pointless and may mess up using kotlin-utils as a dependency)

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -57,29 +57,3 @@ val project = project {
         })
     }
 }
-
-@Suppress("unused")
-@Task(name = "removeJavadocJar", description = "Remove empty Javadoc JAR from output",
-      alwaysRunAfter = arrayOf("assemble"))
-fun taskRemoveJavadocJar(project: Project): TaskResult {
-    //Dokka not configured and is buggy anyway
-    val javadocJar = Paths.get(project.buildDirectory, "libs",
-                               "${project.name}-${project.version}-javadoc.jar").toAbsolutePath()
-    return try {
-        Files.delete(javadocJar)
-        println("Deleted $javadocJar")
-        TaskResult()
-    }
-    catch (except: Exception) {
-        when (except) {
-            is IOException, is SecurityException -> {
-                System.err.println("Caught ${except::class.simpleName} after attempting to delete $javadocJar")
-                except.printStackTrace()
-
-                TaskResult(false, errorMessage = except.localizedMessage)
-            }
-
-            else -> throw except
-        }
-    }
-}
